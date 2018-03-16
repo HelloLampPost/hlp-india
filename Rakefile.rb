6cb53821ef8affd13c61687fc5569410140e5c26
JamesHanks <james.ra.hanks@gmail.com>
2018-03-16 10:58:50 +0000

    Destroy everything

--- Rakefile.rb
+++
@@ -1,6 +0,0 @@
-task :default => [:test]
-
-desc "Run all the tests"
-task :test do
-  Dir.glob('test/**/*_test.rb') { |f| require_relative f }
-end
--- test/test_helper.rb
+++
@@ -1,7 +0,0 @@
-require 'test/unit'
-require 'rack/test'
-require_relative '../app'
-
-def app
-  HelloLamppostWebsite
-end
